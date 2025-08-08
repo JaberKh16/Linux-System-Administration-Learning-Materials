429a58a725f2544e71b14b6cbccc9026ed3f4263
Jaber Khan <md.jaberkhan66@gmail.com>
2025-08-09 05:00:49 +0600

    array-concepts

--- Shell-Scripting-Materials/15.array.sh
+++ Shell-Scripting-Materials/15.array.sh
@@ -5,13 +5,59 @@
 colors=('Red' 'Blue' 'Green')
 
 # access 0th index value
-echo "colors[0]${colors[0]}"; 
+echo "colors[0]${colors[0]}";
 
 # access all values
+# using @ or * will give all values
 echo "colors[] containing values: ${colors[@]}"
+echo "colors[] containing values: ${colors[*]}"
 
 # access indices value
 echo "colors[] indices are: ${!colors[@]}"
 
 # get the length
 echo "Length of colors[]: ${#colors[@]}"
+
+# get values from 1st to 2nd index
+echo "colors[1:2] values are: ${colors[@]:1:2}"
+
+# update the array
+colors[0]="Yellow" # update or replace 0th index value
+echo "Updated colors[0]: ${colors[0]}"
+
+
+# add new value to the array
+colors+=('Black') # add new value to the end of the array
+echo "Added new value to colors[]: ${colors[@]}"
+
+# delete or remove or unset an element from the array
+unset colors[1] # remove 1st index value
+echo "After removing 1st index value: ${colors[@]}"
+
+
+
+# Example:2
+# Key-Value Pair Array
+declare -A person
+# Associative array (key-value pairs)
+person=(
+    [name]="John Doe"
+    [age]=30
+    [city]="New York"
+)
+# Accessing values
+echo "Name: ${person[name]}"
+echo "Age: ${person[age]}"
+echo "City: ${person[city]}"
+
+# access all keys
+echo "Keys: ${!person[@]}"
+# access all values
+echo "Values: ${person[@]}"
+
+# Adding a new key-value pair
+person[country]="USA"
+echo "Country: ${person[country]}"
+# Deleting a key-value pair
+unset person[age]
+echo "After deleting age: ${person[age]}" # This will be empty
--- Shell-Scripting-Materials/16.string.sh
+++ Shell-Scripting-Materials/16.string.sh
@@ -18,7 +18,7 @@ declare -r song="Tada da da.."
 echo -e "Lyrics: $song"
 
 
-# Length of string
+# Example-2: Length of string
 # ${#var_name} here # to find the length
 echo -e "Length of song is: ${#song}"
 
@@ -26,12 +26,73 @@ echo -e "Length of song is: ${#song}"
 
 # read string with 'read'
 # here flag, -p, prompt and goes to next line
-$ read -p 'Type your name and press enter: ' NAME
-$ echo "Hello $NAME" 
+read -p 'Type your name and press enter: ' NAME
+echo "Hello $NAME"
 
 # Example-3: Substring Find
-# colon (:) operator inside the parameter expansion, providing the starting position of substring and 
+# colon (:) operator inside the parameter expansion, providing the starting position of substring and
 # optionally length of the substring
-$ NAME=Baeldung
-$ echo ${NAME:6}
-$ echo ${NAME:0:4}
+NAME=Baeldung
+echo ${NAME:6}
+echo ${NAME:0:4}
+
+
+# Example-4: String Concatenation
+# Concatenation is done by simply placing the strings next to each other
+STR1="Hello"
+STR2="World"
+CONCATENATED="$STR1 $STR2"
+echo $CONCATENATED
+
+
+# Example-5: String Comparison
+# String comparison is done using the `=` operator inside the `[[ ]]` conditional expression
+STR1="Hello"
+STR2="World"
+if [[ $STR1 == $STR2 ]]; then
+    echo "Strings are equal"
+else
+    echo "Strings are not equal"
+fi
+
+
+# Example-6: String Replacement
+# String replacement can be done using the `${variable//search/replace}` syntax
+TEXT="Hello World"
+REPLACED_TEXT=${TEXT//World/Baeldung}
+echo $REPLACED_TEXT
+
+
+# Example-7: String to Uppercase
+# Convert a string to uppercase using the `^^` operator
+TEXT="hello world"
+UPPERCASE_TEXT=${TEXT^^}
+echo $UPPERCASE_TEXT
+
+# Example-8: String to Lowercase
+# Convert a string to lowercase using the `,,` operator
+TEXT="HELLO WORLD"
+LOWERCASE_TEXT=${TEXT,,}
+echo $LOWERCASE_TEXT
+
+
+# Example-9: String Splitting
+# Split a string into an array using the `IFS` (Internal Field Separator)
+IFS=' ' read -r -a ARRAY <<< "Hello World from Baeldung"
+echo "Array elements:"
+for ELEMENT in "${ARRAY[@]}"; do
+    echo $ELEMENT
+done
+
+# Example-10: String Trimming
+# Trim leading and trailing whitespace using `sed`
+TEXT="   Hello World   "
+TRIMMED_TEXT=$(echo "$TEXT" | sed 's/^[ \t]*//; s/[ \t]*$//')
+echo "Trimmed text: '$TRIMMED_TEXT'"
+
+
+# Example-11: String Reversal
+# Reverse a string using `rev` command
+TEXT="Hello World"
+REVERSED_TEXT=$(echo "$TEXT" | rev)
+echo "Reversed text: $REVERSED_TEXT"
