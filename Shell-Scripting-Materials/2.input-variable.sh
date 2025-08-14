e320ac9d460b0317117628dea2882da6351ac85e
Jaber Khan <md.jaberkhan66@gmail.com>
2025-08-14 23:48:55 +0600

    Variables-Concepts

---
+++ Shell-Scripting-Materials/2.input-variable.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# This script prompts the user to enter a message and then prints it.
+echo "Enter a message:"
+read msg
+
+# read -p "Enter a message: " msg # read -p with prompt
+echo $msg
